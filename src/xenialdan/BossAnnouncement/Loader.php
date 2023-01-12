387df7b27714ef0301c8b1cb379d18ba6f72b5e2
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2023-01-12 18:13:36 +0700

    Test

--- src/xenialdan/BossAnnouncement/Loader.php
+++ src/xenialdan/BossAnnouncement/Loader.php
@@ -19,7 +19,7 @@ use pocketmine\utils\TextFormat;
 use xenialdan\apibossbar\DiverseBossBar;
 
 class Loader extends PluginBase implements Listener {
-    
+
     /** @var DiverseBossBar */
     public $bar;
     public $title = '', $subTitles = [], $changeSpeed = 0, $i = 0;
@@ -100,7 +100,7 @@ class Loader extends PluginBase implements Listener {
         // preg_match_all ("/(\{.*?\})/ig", $text, $brackets);
 
         //TODO auto function
-        $text = str_replace(['{display_name}', '{name}', '{x}', '{y}', '{z}', '{world}', '{level_players}', '{server_players}', '{server_max_players}', '{hour}', '{minute}', '{second}', '{BLACK}', '{DARK_BLUE}', '{DARK_GREEN}', '{DARK_AQUA}', '{DARK_RED}', '{DARK_PURPLE}', '{GOLD}', '{GRAY}', '{DARK_GRAY}', '{BLUE}', '{GREEN}', '{AQUA}', '{RED}', '{LIGHT_PURPLE}', '{YELLOW}', '{WHITE}', '{OBFUSCATED}', '{BOLD}', '{STRIKETHROUGH}', '{UNDERLINE}', '{ITALIC}', '{RESET}', '&0', '&1', '&2', '&3', '&4', '&5', '&6', '&7', '&8', '&9', '&a', '&b', '&c', '&d', '&e', '&f', '&k', '&l', '&m', '&n', '&o', '&r'], [$player->getDisplayName(), $player->getName(), $player->getFloorX(), $player->getFloorY(), $player->getFloorZ(), ($level = $player->getLevel()) !== null ? $level->getName() : '', count($player->getLevel()->getPlayers()), count($player->getServer()->getOnlinePlayers()), $player->getServer()->getMaxPlayers(), date('H'), date('i'), date('s'), '&0', '&1', '&2', '&3', '&4', '&5', '&6', '&7', '&8', '&9', '&a', '&b', '&c', '&d', '&e', '&f', '&k', '&l', '&m', '&n', '&o', '&r', TextFormat::BLACK, TextFormat::DARK_BLUE, TextFormat::DARK_GREEN, TextFormat::DARK_AQUA, TextFormat::DARK_RED, TextFormat::DARK_PURPLE, TextFormat::GOLD, TextFormat::GRAY, TextFormat::DARK_GRAY, TextFormat::BLUE, TextFormat::GREEN, TextFormat::AQUA, TextFormat::RED, TextFormat::LIGHT_PURPLE, TextFormat::YELLOW, TextFormat::WHITE, TextFormat::OBFUSCATED, TextFormat::BOLD, TextFormat::STRIKETHROUGH, TextFormat::UNDERLINE, TextFormat::ITALIC, TextFormat::RESET], $text);
+        $text = str_replace(['{display_name}', '{name}', '{x}', '{y}', '{z}', '{world}', '{level_players}', '{server_players}', '{server_max_players}', '{hour}', '{minute}', '{second}', '{BLACK}', '{DARK_BLUE}', '{DARK_GREEN}', '{DARK_AQUA}', '{DARK_RED}', '{DARK_PURPLE}', '{GOLD}', '{GRAY}', '{DARK_GRAY}', '{BLUE}', '{GREEN}', '{AQUA}', '{RED}', '{LIGHT_PURPLE}', '{YELLOW}', '{WHITE}', '{OBFUSCATED}', '{BOLD}', '{STRIKETHROUGH}', '{UNDERLINE}', '{ITALIC}', '{RESET}', '&0', '&1', '&2', '&3', '&4', '&5', '&6', '&7', '&8', '&9', '&a', '&b', '&c', '&d', '&e', '&f', '&k', '&l', '&m', '&n', '&o', '&r'], [$player->getDisplayName(), $player->getName(), $player->getPosition()->getFloorX(), $player->getPosition()->getFloorY(), $player->getPosition()->getFloorZ(), ($level = $player->getWorld()) !== null ? $level->getFolderName() : '', count($player->getWorld()->getPlayers()), count($this->getServer()->getOnlinePlayers()), $this->getServer()->getMaxPlayers(), date('H'), date('i'), date('s'), '&0', '&1', '&2', '&3', '&4', '&5', '&6', '&7', '&8', '&9', '&a', '&b', '&c', '&d', '&e', '&f', '&k', '&l', '&m', '&n', '&o', '&r', TextFormat::BLACK, TextFormat::DARK_BLUE, TextFormat::DARK_GREEN, TextFormat::DARK_AQUA, TextFormat::DARK_RED, TextFormat::DARK_PURPLE, TextFormat::GOLD, TextFormat::GRAY, TextFormat::DARK_GRAY, TextFormat::BLUE, TextFormat::GREEN, TextFormat::AQUA, TextFormat::RED, TextFormat::LIGHT_PURPLE, TextFormat::YELLOW, TextFormat::WHITE, TextFormat::OBFUSCATED, TextFormat::BOLD, TextFormat::STRIKETHROUGH, TextFormat::UNDERLINE, TextFormat::ITALIC, TextFormat::RESET], $text);
 
         return $text;
     }
@@ -109,8 +109,7 @@ class Loader extends PluginBase implements Listener {
      * @param string $levelName
      * @return bool
      */
-    public function isWorldEnabled(string $levelName): bool
-    {
+    public function isWorldEnabled(string $levelName) : bool {
         $mode = $this->getConfig()->get('mode', 0);
         $configWorlds = array_map(static function (string $worldName): string {
             return strtolower(TextFormat::clean($worldName));
